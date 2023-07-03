371603475975a52a4323eeb4dc4aee68ce790bac
IsabelGouveia <131471952+IsabelGouveia@users.noreply.github.com>
2023-07-03 17:46:44 +0000

    random card generator done

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".card").classList.add(getRandomSuit());
+  document.querySelector(".number").innerHTML = getRandomNumber();
+};
+
+let getRandomNumber = () => {
+  let number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "Q",
+    "J",
+    "K"
+  ];
+  let indexNumber = Math.floor(Math.random() * number.length);
+  return number[indexNumber];
+};
+
+let getRandomSuit = () => {
+  let suit = ["diamond", "heart", "club", "spade"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
 };
